af8e4f3d9b007e6140e709abba9bddac04eec779
Sam19880! <sam497287@gmail.com>
2025-03-21 09:39:07 +0530

    Pushing the code

--- src/ex_01_Java_Basic/Lab001_HelloWorldprogram.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorldprogram.java
@@ -1,6 +1,6 @@
 package ex_01_Java_Basic;
 
-public class Lab001_Helloworldprogram {
+public class Lab001_HelloWorldprogram {
     public static void main(String[] args) {
         System.out.println("hello");
     }
